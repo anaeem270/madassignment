ea4eda80c12f1d6beacd4ba7eed9e9bde993e68c
Arslan Naeem <anaeem270@gmail.com>
2021-01-30 19:34:27 +1100

    Up datint

---
+++ app/src/main/java/com/example/covid19symptonrecorder/CompareCovid.java
@@ -0,0 +1,14 @@
+package com.example.covid19symptonrecorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompareCovid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compare_covid);
+    }
+}
